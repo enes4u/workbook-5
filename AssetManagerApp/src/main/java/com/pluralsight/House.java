ac8c8c817065a19a258ae66493fbad70c9902324
enes4u <enesuzunyearup@gmail.com>
2025-05-13 11:57:57 -0400

    structure in asset manager

---
+++ AssetManagerApp/src/main/java/com/pluralsight/House.java
@@ -0,0 +1,44 @@
+package com.pluralsight;
+
+public class House extends Asset {
+    private String address;
+    private int condition;
+    private int squareFoot;
+    private int lotSize;
+
+    // Constructor
+    public House(String description, String dateAcquired, double originalCost, String address, int condition, int squareFoot, int lotSize) {
+        super(description, dateAcquired, originalCost);
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+
+    // Getters and Setters
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public int getCondition() { return condition; }
+    public void setCondition(int condition) { this.condition = condition; }
+
+    public int getSquareFoot() { return squareFoot; }
+    public void setSquareFoot(int squareFoot) { this.squareFoot = squareFoot; }
+
+    public int getLotSize() { return lotSize; }
+    public void setLotSize(int lotSize) { this.lotSize = lotSize; }
+
+    // Override getValue() method
+    @Override
+    public double getValue() {
+        double pricePerSquareFoot;
+        switch (condition) {
+            case 1: pricePerSquareFoot = 180.00; break;
+            case 2: pricePerSquareFoot = 130.00; break;
+            case 3: pricePerSquareFoot = 90.00; break;
+            case 4: pricePerSquareFoot = 80.00; break;
+            default: pricePerSquareFoot = 0.00; break;
+        }
+        return (squareFoot * pricePerSquareFoot) + (lotSize * 0.25);
+    }
+}
